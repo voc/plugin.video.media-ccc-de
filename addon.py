cfeeb0fea68fe29ebb3ad694fb8777679c2bdef5
Tobias Gruetzmacher <tobias-git@23.gs>
2015-08-12 22:03:59 +0200

    Add live streaming support.

--- addon.py
+++ addon.py
@@ -6,9 +6,13 @@ import requests
 from xbmcswift2 import Plugin
 
 from resources.lib.helpers import recording_list
+from resources.lib.stream import Streams
 
-BASE_URL = 'http://api.media.ccc.de/public/'
+BASE_URL = 'https://api.media.ccc.de/public/'
+LIVE_URL = 'http://streaming.media.ccc.de/streams/v1.json'
+
 #BASE_URL = 'http://127.0.0.1:3000/public/'
+#LIVE_URL = 'http://127.0.0.1:3000/v1.json'
 
 plugin = Plugin()
 
@@ -46,7 +50,15 @@ def show_dir(subdir = ''):
                 'path': plugin.url_for('show_conference', conf = event['url'].rsplit('/', 1)[1])
             })
 
-    return sorted(items, key=operator.itemgetter('label'))
+    items.sort(key=operator.itemgetter('label'))
+
+    if depth == 0:
+        items.insert(0, {
+            'label': 'Live Streaming',
+            'path': plugin.url_for('show_live')
+        })
+
+    return items
 
 @plugin.route('/conference/<conf>')
 def show_conference(conf):
@@ -86,6 +98,33 @@ def resolve_event(event, quality = None, format = None):
     if len(want) > 0:
         requests.post(BASE_URL + 'recordings/count', data = {'event_id': event, 'src': want[0].url})
         plugin.set_resolved_url(want[0].url)
+
+@plugin.route('/live')
+def show_live():
+    quality = QUALITY[plugin.get_setting('quality', int)]
+    format = FORMATS[plugin.get_setting('format', int)]
+
+    req = requests.get(LIVE_URL)
+    data = Streams(req.json())
+
+    if len(data.rooms) == 0:
+        return [{
+            'label': 'No live event currently, go watch some recordings!',
+            'path': plugin.url_for('index')
+        }]
+
+    items = []
+    for room in data.rooms:
+        want = room.streams_sorted(quality, format)
+
+        if len(want) > 0:
+            items.append({
+                'label': room.display,
+                'is_playable': True,
+                'path': want[0].url
+            })
+    return items
+
 
 @plugin.cached()
 def get_index_data():
