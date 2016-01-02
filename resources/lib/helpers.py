958064359812efb0ae769ea3bab9848e544abd58
Tobias Gruetzmacher <tobias-git@23.gs>
2016-01-02 18:34:24 +0100

    Reverse reversed priority logic.

--- resources/lib/helpers.py
+++ resources/lib/helpers.py
@@ -30,42 +30,42 @@ class Recording(object):
 
 def user_preference_sorter(prefer_quality, prefer_format):
     def do_sort(obj):
-        prio = 100
+        prio = 0
 
         if obj.format == prefer_format:
-            prio -= 20
+            prio += 20
 
         # Bonus & penalty for exact matches, no score for "obj.hd == None"
         if obj.hd == True and prefer_quality == "hd":
-            prio -= 20
+            prio += 20
         elif obj.hd == False and prefer_quality == "sd":
-            prio -= 20
+            prio += 20
         elif obj.hd == True and prefer_quality == "sd":
-            prio += 10
+            prio -= 10
         elif obj.hd == False and prefer_quality == "hd":
-            prio += 10
+            prio -= 10
 
         # "web" versions are missing one audio track
         # (legacy, but not all conferences have proper language tags yet)
         try:
             if obj.orig_mime.endswith('-web'):
-                prio += 5
+                prio -= 5
         except AttributeError:
             pass
 
         # Prefer versions with "more" audio tracks
         try:
             translations = len(obj.languages) - 1
-            prio -= translations
+            prio += translations
         except AttributeError:
             pass
 
         # Prefer "native" over "translated" for now (streaming)...
         try:
             if obj.translated:
-                prio -= 5
+                prio += 5
         except AttributeError:
             pass
 
-        return prio
+        return -prio
     return do_sort
